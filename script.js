e264cee7dbb7f193c3de37a26ff61dfc1bb3ad18
Md Rijoan Maruf <78620963+mdrijoanmaruf@users.noreply.github.com>
2021-10-30 01:42:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,116 +1,67 @@
-const selectBox = document.querySelector(".select-box"),
-selectBtnX = selectBox.querySelector(".options .playerX"),
-selectBtnO = selectBox.querySelector(".options .playerO"),
-playBoard = document.querySelector(".play-board"),
-players = document.querySelector(".players"),
-allBox = document.querySelectorAll("section span"),
-resultBox = document.querySelector(".result-box"),
-wonText = resultBox.querySelector(".won-text"),
-replayBtn = resultBox.querySelector("button");
+console.log("Welcome to Tic Tac Toe")
+let music = new Audio("music.mp3")
+let audioTurn = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
+let turn = "X"
+let isgameover = false;
 
-window.onload = ()=>{ //once window loaded
-    for (let i = 0; i < allBox.length; i++) { //add onclick attribute in all available span
-       allBox[i].setAttribute("onclick", "clickedBox(this)");
-    }
+// Function to change the turn
+const changeTurn = ()=>{
+    return turn === "X"? "0": "X"
 }
 
-selectBtnX.onclick = ()=>{
-    selectBox.classList.add("hide"); //hide select box
-    playBoard.classList.add("show"); //show the playboard section
-}
-
-selectBtnO.onclick = ()=>{ 
-    selectBox.classList.add("hide"); //hide select box
-    playBoard.classList.add("show"); //show the playboard section
-    players.setAttribute("class", "players active player"); //set class attribute in players with players active player values
-}
-
-let playerXIcon = "fas fa-times"; //class name of fontawesome cross icon
-let playerOIcon = "far fa-circle"; //class name of fontawesome circle icon
-let playerSign = "X"; //this is a global variable beacuse we've used this variable inside multiple functions
-let runBot = true; //this also a global variable with boolen value..we used this variable to stop the bot once match won by someone or drawn
-
-// user click function
-function clickedBox(element){
-    if(players.classList.contains("player")){
-        playerSign = "O"; //if player choose (O) then change playerSign to O
-        element.innerHTML = `<i class="${playerOIcon}"></i>`; //adding circle icon tag inside user clicked element/box
-        players.classList.remove("active"); ///add active class in players
-        element.setAttribute("id", playerSign); //set id attribute in span/box with player choosen sign
-    }else{
-        element.innerHTML = `<i class="${playerXIcon}"></i>`; //adding cross icon tag inside user clicked element/box
-        element.setAttribute("id", playerSign); //set id attribute in span/box with player choosen sign
-        players.classList.add("active"); ///add active class in players
-    }
-    selectWinner(); //calling selectWinner function
-    element.style.pointerEvents = "none"; //once user select any box then that box can'be clicked again
-    playBoard.style.pointerEvents = "none"; //add pointerEvents none to playboard so user can't immediately click on any other box until bot select
-    let randomTimeDelay = ((Math.random() * 1000) + 200).toFixed(); //generating random number so bot will randomly delay to select unselected box
-    setTimeout(()=>{
-        bot(runBot); //calling bot function
-    }, randomTimeDelay); //passing random delay value
-}
-
-// bot auto select function
-function bot(){
-    let array = []; //creating empty array...we'll store unclicked boxes index
-    if(runBot){ //if runBot is true
-        playerSign = "O"; //change the playerSign to O so if player has chosen X then bot will O
-        for (let i = 0; i < allBox.length; i++) {
-            if(allBox[i].childElementCount == 0){ //if the box/span has no children means <i> tag
-                array.push(i); //inserting unclicked boxes number/index inside array
-            }
+// Function to check for a win
+const checkWin = ()=>{
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+        [0, 3, 6, -5, 15, 90],
+        [1, 4, 7, 5, 15, 90],
+        [2, 5, 8, 15, 15, 90],
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135],
+    ]
+    wins.forEach(e =>{
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
+            isgameover = true
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+            document.querySelector(".line").style.width = "20vw";
         }
-        let randomBox = array[Math.floor(Math.random() * array.length)]; //getting random index from array so bot will select random unselected box
-        if(array.length > 0){ //if array length is greater than 0
-            if(players.classList.contains("player")){ 
-                playerSign = "X"; //if player has chosen O then bot will X
-                allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`; //adding cross icon tag inside bot selected element
-                allBox[randomBox].setAttribute("id", playerSign); //set id attribute in span/box with player choosen sign
-                players.classList.add("active"); ///add active class in players
-            }else{
-                allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`; //adding circle icon tag inside bot selected element
-                players.classList.remove("active"); //remove active class in players
-                allBox[randomBox].setAttribute("id", playerSign); //set id attribute in span/box with player choosen sign
-            }
-            selectWinner(); //calling selectWinner function
-        }
-        allBox[randomBox].style.pointerEvents = "none"; //once bot select any box then user can't click on that box
-        playBoard.style.pointerEvents = "auto"; //add pointerEvents auto in playboard so user can again click on box
-        playerSign = "X"; //if player has chosen X then bot will be O right then we change the playerSign again to X so user will X because above we have changed the playerSign to O for bot
-    }
+    })
 }
 
-function getIdVal(classname){
-    return document.querySelector(".box" + classname).id; //return id value
-}
-function checkIdSign(val1, val2, val3, sign){ //checking all id value is equal to sign (X or O) or not if yes then return true
-    if(getIdVal(val1) == sign && getIdVal(val2) == sign && getIdVal(val3) == sign){
-        return true;
-    }
-}
-function selectWinner(){ //if the one of following winning combination match then select the winner
-    if(checkIdSign(1,2,3,playerSign) || checkIdSign(4,5,6, playerSign) || checkIdSign(7,8,9, playerSign) || checkIdSign(1,4,7, playerSign) || checkIdSign(2,5,8, playerSign) || checkIdSign(3,6,9, playerSign) || checkIdSign(1,5,9, playerSign) || checkIdSign(3,5,7, playerSign)){
-        runBot = false; //passing the false boolen value to runBot so bot won't run again
-        bot(runBot); //calling bot function
-        setTimeout(()=>{ //after match won by someone then hide the playboard and show the result box after 700ms
-            resultBox.classList.add("show");
-            playBoard.classList.remove("show");
-        }, 700); //1s = 1000ms
-        wonText.innerHTML = `Player <p>${playerSign}</p> won the game!`; //displaying winning text with passing playerSign (X or O)
-    }else{ //if all boxes/element have id value and still no one win then draw the match
-        if(getIdVal(1) != "" && getIdVal(2) != "" && getIdVal(3) != "" && getIdVal(4) != "" && getIdVal(5) != "" && getIdVal(6) != "" && getIdVal(7) != "" && getIdVal(8) != "" && getIdVal(9) != ""){
-            runBot = false; //passing the false boolen value to runBot so bot won't run again
-            bot(runBot); //calling bot function
-            setTimeout(()=>{ //after match drawn then hide the playboard and show the result box after 700ms
-                resultBox.classList.add("show");
-                playBoard.classList.remove("show");
-            }, 700); //1s = 1000ms
-            wonText.textContent = "Match has been drawn!"; //displaying draw match text
+// Game Logic
+// music.play()
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element =>{
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', ()=>{
+        if(boxtext.innerText === ''){
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            audioTurn.play();
+            checkWin();
+            if (!isgameover){
+                document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+            } 
         }
-    }
-}
+    })
+})
 
-replayBtn.onclick = ()=>{
-    window.location.reload(); //reload the current page on replay button click
-}
+// Add onclick listener to reset button
+reset.addEventListener('click', ()=>{
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = ""
+    });
+    turn = "X"; 
+    isgameover = false
+    document.querySelector(".line").style.width = "0vw";
+    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
+})
+
